d298525a5ff124b65e47d0343baa50eb8f14b5ef
thxto-me <hjd0907@gmail.com>
2023-04-05 15:55:04 +0900

    Status Update (Apr. 05, 2023. 15:54)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -3,55 +3,48 @@ package kr.ac.gachon.mop_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity { // implements Button.OnClickListener{
-    private View layoutLogin;
-    private View layoutInfo;
-    private View layoutGrade;
+    String tag = "LifeCycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        layoutLogin = (LinearLayout) findViewById(R.id.layoutLogin);
-        layoutInfo = (LinearLayout) findViewById(R.id.layoutInfo);
-        layoutGrade = (LinearLayout) findViewById(R.id.layoutGrade);
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        Button btnInfo = (Button) findViewById(R.id.btnInfo);
-        Button btnGrade = (Button) findViewById(R.id.btnGrade);
+        Log.d(tag, "In the onCreate() event");
+    }
 
-        layoutLogin.setVisibility(View.VISIBLE);
-        layoutInfo.setVisibility(View.INVISIBLE);
-        layoutGrade.setVisibility(View.INVISIBLE);
-        // login button
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutLogin.setVisibility(View.VISIBLE);
-                layoutInfo.setVisibility(View.INVISIBLE);
-                layoutGrade.setVisibility(View.INVISIBLE);
-            }
-        });
-        // info button
-        btnInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutLogin.setVisibility(View.INVISIBLE);
-                layoutInfo.setVisibility(View.VISIBLE);
-                layoutGrade.setVisibility(View.INVISIBLE);
-            }
-        });
-        // grade button
-        btnGrade.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                layoutLogin.setVisibility(View.INVISIBLE);
-                layoutInfo.setVisibility(View.INVISIBLE);
-                layoutGrade.setVisibility(View.VISIBLE);
-            }
-        });
+    public void onStart() {
+        super.onStart();
+        Log.d(tag, "In the onStart() event");
+    }
+
+    public void onRestart() {
+        super.onRestart();
+        Log.d(tag, "In the onRestart() event");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(tag, "In the onResume() event");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(tag, "In the onPause() event");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(tag, "In the onStop() event");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "In the onDestroy() event");
     }
 }
