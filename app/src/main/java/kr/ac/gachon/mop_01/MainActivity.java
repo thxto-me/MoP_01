7d26614e6bcb91eaf5dc5090eef0dd70a523b733
thxto-me <hjd0907@gmail.com>
2023-04-06 11:42:24 +0900

    Status Update (Apr. 06, 2023. 11:42)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -2,49 +2,31 @@ package kr.ac.gachon.mop_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity { // implements Button.OnClickListener{
-    String tag = "LifeCycle";
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(tag, "In the onCreate() event");
-    }
-
-    public void onStart() {
-        super.onStart();
-        Log.d(tag, "In the onStart() event");
-    }
-
-    public void onRestart() {
-        super.onRestart();
-        Log.d(tag, "In the onRestart() event");
-    }
-
-    public void onResume() {
-        super.onResume();
-        Log.d(tag, "In the onResume() event");
-    }
-
-    public void onPause() {
-        super.onPause();
-        Log.d(tag, "In the onPause() event");
-    }
-
-    public void onStop() {
-        super.onStop();
-        Log.d(tag, "In the onStop() event");
-    }
-
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(tag, "In the onDestroy() event");
+        Button btnStart = findViewById(R.id.btnStartService);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(getApplicationContext(), MyService.class));
+            }
+        });
+        Button btnStop = findViewById(R.id.btnStopService);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getApplicationContext(), MyService.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/kr/ac/gachon/mop_01/MyService.java
@@ -0,0 +1,34 @@
+package kr.ac.gachon.mop_01;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
