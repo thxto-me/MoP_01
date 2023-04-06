5f9bd953294eb047882634323e0994065f257647
thxto-me <hjd0907@gmail.com>
2023-04-06 23:23:09 +0900

    Status Update (Apr. 06, 2023. 23:22)
    Lab4-2

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -8,39 +8,57 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    TextView labelUserName;
-    EditText txtUserName;
-    Button btnBegin;
-    private Context context;
-    private int duration = Toast.LENGTH_SHORT;
+    CheckBox chkCream;
+    CheckBox chkSugar;
+    Button btnPay;
+    RadioGroup radCoffeeType;
+    RadioButton radDecaf;
+    RadioButton radEspresso;
+    RadioButton radColombian;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = getApplicationContext();
-        labelUserName = findViewById(R.id.TextView1);
-        txtUserName = findViewById(R.id.txtUserName);
-        btnBegin = findViewById(R.id.btn1);
-        btnBegin.setOnClickListener(new View.OnClickListener() {
+
+        chkCream = findViewById(R.id.chkCream);
+        chkSugar = findViewById(R.id.chkSugar);
+        btnPay = findViewById(R.id.btnPay);
+        radCoffeeType = findViewById(R.id.radGroupCoffeeType);
+        radDecaf = findViewById(R.id.radDecaf);
+        radEspresso = findViewById(R.id.radExpresso);
+        radColombian = findViewById(R.id.radColombian);
+        btnPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String userName = txtUserName.getText().toString();
-                if (userName.compareTo("Michael Han") == 0) {
-                    labelUserName.setText("OK, Please wait..");
-                    Toast.makeText(context, "Hi!, Mr." + userName, duration).show();
-                } else {
-                    Toast.makeText(context, userName + " is not a valid User", duration).show();
+                String message = "Coffee ";
+                if (chkCream.isChecked()) {
+                    message += " & cream ";
                 }
+                if (chkSugar.isChecked()) {
+                    message += " & Sugar";
+                }
+                int radioId =
+                        radCoffeeType.getCheckedRadioButtonId();
+                if (radDecaf.getId() == radioId)
+                    message = "Decaf " + message;
+                if (radColombian.getId() == radioId)
+                    message = "Colombian " + message;
+                if (radEspresso.isChecked())
+                    message = "Espresso " + message;
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
             }
         });
     }
