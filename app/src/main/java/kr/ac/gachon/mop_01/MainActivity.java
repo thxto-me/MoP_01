21da7363d1f78e4d968e79643a96245203174580
thxto-me <hjd0907@gmail.com>
2023-04-20 21:32:33 +0900

    Status Update (Apr. 20, 2023. 21:32)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -3,19 +3,44 @@ package kr.ac.gachon.mop_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.sql.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    String[] items = {"mike", "angel", "crow", "john", "ginnie", "sally", "cohen", "rice"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] names = {"Lee", "Choi", "Kim", "Palk", "Han"};
-        ListView listView = (ListView) findViewById(R.id.listview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
-        listView.setAdapter(adapter);
+        textView = (TextView) findViewById(R.id.textView);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapterView, View view, int position, long id) {
+                textView.setText(items[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView parent) {
+                textView.setText("");
+            }
+        });
     }
 }
