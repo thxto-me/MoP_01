adf50379fff90be8eb7c1b30264eb5db1bb24acf
thxto-me <hjd0907@gmail.com>
2023-05-03 22:40:04 +0900

    Status Update (Mar. 03, 2023. 22:39)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,16 +1,8 @@
 package kr.ac.gachon.mop_01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
-import java.sql.Array;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
