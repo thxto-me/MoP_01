2fd8ff76a21e71de606fb8adb1924076759121dc
thxto-me <hjd0907@gmail.com>
2023-05-03 15:39:26 +0900

    Status Update (Mar. 03, 2023. 15:39

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -14,33 +14,9 @@ import java.sql.Array;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    String[] items = {"mike", "angel", "crow", "john", "ginnie", "sally", "cohen", "rice"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.textView);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView adapterView, View view, int position, long id) {
-                textView.setText(items[position]);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView parent) {
-                textView.setText("");
-            }
-        });
     }
 }
