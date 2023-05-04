0a167325fca1496ff07b51bb66346d93b952d163
thxto-me <hjd0907@gmail.com>
2023-05-04 12:46:59 +0900

    Status Update (Mar. 04, 2023. 12:42)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,14 +1,32 @@
 package kr.ac.gachon.mop_01;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView1;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1 = findViewById(R.id.imageView1);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button1);
+
+        button.setOnClickListener(v -> {
+            Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate);
+            imageView1.startAnimation(anim);
+            editText.append("Animation Started\n");
+        });
     }
 }
