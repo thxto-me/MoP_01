9d8a5c3289e7ad989bf03e7879caa8089244a122
thxto-me <hjd0907@gmail.com>
2023-05-16 13:46:03 +0900

    Status Update (Mar. 15, 2023. 13:45)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,8 +1,8 @@
 package kr.ac.gachon.mop_01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -12,33 +12,32 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     TextView textview0;
     ProgressBar bar;
-    ProgressHandler handler;
+    Handler handler;
+    ProgressRunnable runnable;
     boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview0 = (TextView) findViewById(R.id.TextView01);
-        bar = (ProgressBar) findViewById(R.id.progress);
-        handler = new ProgressHandler();
+        textview0 = findViewById(R.id.TextView01);
+        bar = findViewById(R.id.progress);
+        handler = new Handler();
+        runnable = new ProgressRunnable();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         bar.setProgress(0);
-        Thread thread1 = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    for (int i = 0; i < 20 && isRunning; i++) {
-                        Thread.sleep(1000);
-                        Message msg = handler.obtainMessage();
-                        handler.sendMessage(msg);
-                    }
-                } catch (Exception e) {
-                    Log.e("MainActivity", "Exception in processing Message", e);
+        Thread thread1 = new Thread(() -> {
+            try {
+                for (int i = 0; i < 20 && isRunning; i++) {
+                    Thread.sleep(1000);
+                    handler.post(runnable);
                 }
+            } catch (Exception e) {
+                Log.e("MainActivity", "Exception in processing Message", e);
             }
         });
 
@@ -53,8 +52,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public class ProgressHandler extends Handler {
-        public void handleMessage(Message msg) {
+    public class ProgressRunnable implements Runnable {
+
+        @SuppressLint("SetTextI18n")
+        public void run() {
             bar.incrementProgressBy(5);
             if (bar.getProgress() == bar.getMax()) {
                 textview0.setText("Done");
