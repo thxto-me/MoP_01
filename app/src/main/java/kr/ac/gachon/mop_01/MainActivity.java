d727d2f7b1ee9d1a89b98b658a2203e199008bb4
thxto-me <hjd0907@gmail.com>
2023-05-16 14:00:36 +0900

    Status Update (Mar. 15, 2023. 13:59)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,67 +1,53 @@
 package kr.ac.gachon.mop_01;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textview0;
-    ProgressBar bar;
-    Handler handler;
-    ProgressRunnable runnable;
-    boolean isRunning = false;
+    TextView tvCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview0 = findViewById(R.id.TextView01);
-        bar = findViewById(R.id.progress);
-        handler = new Handler();
-        runnable = new ProgressRunnable();
+
+        tvCounter = findViewById(R.id.tv_counter);
+        new CountDownTask().execute();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        bar.setProgress(0);
-        Thread thread1 = new Thread(() -> {
-            try {
-                for (int i = 0; i < 20 && isRunning; i++) {
+    private class CountDownTask extends AsyncTask<Void, Integer, Void> {
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPreExecute() {
+            tvCounter.setText("*start*");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            for (int i = 15; i >= 0; i--) {
+                try {
                     Thread.sleep(1000);
-                    handler.post(runnable);
+                    publishProgress(i);
+                } catch (Exception ignored) {
                 }
-            } catch (Exception e) {
-                Log.e("MainActivity", "Exception in processing Message", e);
             }
-        });
-
-        isRunning = true;
-        thread1.start();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        isRunning = false;
-    }
-
-
-    public class ProgressRunnable implements Runnable {
+            return null;
+        }
 
         @SuppressLint("SetTextI18n")
-        public void run() {
-            bar.incrementProgressBy(5);
-            if (bar.getProgress() == bar.getMax()) {
-                textview0.setText("Done");
-            } else {
-                textview0.setText("Working..." + bar.getProgress());
-            }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            tvCounter.setText(Integer.toString(values[0]));
+        }
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            tvCounter.setText("*Done*");
         }
     }
 }
