e912f446fd9f8a1ed497c69d095bc941f7ead010
thxto-me <hjd0907@gmail.com>
2023-05-16 12:44:39 +0900

    Status Update (Mar. 15, 2023. 12:44)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,32 +1,53 @@
 package kr.ac.gachon.mop_01;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imageView1;
-    EditText editText;
-    Button button;
+    EditText userinput, passinput;
+    Button submit, exit;
+    String username, password;
+    SharedPreferences sh_pref;
+    SharedPreferences.Editor toEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView1 = findViewById(R.id.imageView1);
-        editText = findViewById(R.id.editText);
-        button = findViewById(R.id.button1);
-
-        button.setOnClickListener(v -> {
-            Animation anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate);
-            imageView1.startAnimation(anim);
-            editText.append("Animation Started\n");
+        submit = findViewById(R.id.submit);
+        exit = findViewById(R.id.exit);
+        userinput = findViewById(R.id.userinput);
+        passinput = findViewById(R.id.passinput);
+        submit.setOnClickListener(view -> {
+            username = userinput.getText().toString();
+            password = passinput.getText().toString();
+            sharedPreferences();
+            Toast.makeText(getApplicationContext(), "Details are saved", Toast.LENGTH_LONG).show();
         });
+        exit.setOnClickListener(view -> finish());
+        applySharedPreference();
     }
+
+    private void sharedPreferences() {
+        sh_pref = getSharedPreferences("Login Credentials", MODE_PRIVATE);
+        toEdit = sh_pref.edit();
+        toEdit.putString("Username", username);
+        toEdit.putString("Password", password);
+        toEdit.commit();
+    }
+
+    private void applySharedPreference() {
+        sh_pref = getSharedPreferences("Login Credentials", MODE_PRIVATE);
+        if (sh_pref != null && sh_pref.contains("Username")) {
+            String name = sh_pref.getString("Username", "noname");
+            userinput.setText(name);
+        }
+    }
+
 }
