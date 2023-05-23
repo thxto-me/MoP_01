ca42ffc45e334c7a7cd2097cbcccb499e4d2ccf0
thxto-me <hjd0907@gmail.com>
2023-05-23 21:43:51 +0900

    Status Update (Mar. 23, 2023. 21:42)

--- app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
+++ app/src/main/java/kr/ac/gachon/mop_01/MainActivity.java
@@ -1,54 +1,127 @@
 package kr.ac.gachon.mop_01;
 
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    TextView tvCounter;
+
+    String url = "https://upload.wikimedia.org/wikipedia/commons/3/33/Klaviersonate_Nr._24_Fis-Dur_A_Therese_-_Adagio_cantabile_-_Allegro_ma_non_troppo.ogg";
+    MediaPlayer player;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvCounter = findViewById(R.id.tv_counter);
-        new CountDownTask().execute();
+        //Play
+        Button button1 = findViewById(R.id.btn1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAudio();
+            }
+        });
+
+        //Stop
+        Button button2 = findViewById(R.id.btn2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAudio();
+            }
+        });
+
+        //Pause
+        Button button3 = findViewById(R.id.btn3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseAudio();
+            }
+        });
+
+        //Restart
+        Button button4 = findViewById(R.id.btn4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restartAudio();
+            }
+        });
+
     }
 
-    private class CountDownTask extends AsyncTask<Void, Integer, Void> {
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPreExecute() {
-            tvCounter.setText("*start*");
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            for (int i = 15; i >= 0; i--) {
-                try {
-                    Thread.sleep(1000);
-                    publishProgress(i);
-                } catch (Exception ignored) {
-                }
+    private void restartAudio() {
+        showToast("음악 파일 재생 다시 시작됨");
+        if (player != null && !player.isPlaying()) {
+            try {
+                player.start();
+                player.seekTo(position);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            return null;
         }
 
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            tvCounter.setText(Integer.toString(values[0]));
-        }
+    }
 
-        @SuppressLint("SetTextI18n")
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            tvCounter.setText("*Done*");
+    private void pauseAudio() {
+        showToast("음악 파일 재생 일시 중지됨");
+        if (player != null) {
+            try {
+                position = player.getCurrentPosition();
+                player.pause();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void stopAudio() {
+        showToast("음악 파일 재생 중지됨");
+        if (player != null) {
+            try {
+                player.stop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void playAudio() {
+        showToast("음악 파일 재생 호출됨");
+
+        killPlayer();
+
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(url);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void killPlayer() {
+        if (player != null) {
+            try {
+                player.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
 
